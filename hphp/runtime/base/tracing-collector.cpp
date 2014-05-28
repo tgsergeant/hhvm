d1a0b5788b8d3bdb34684c33be5b724aba7232cf
Tim Sergeant <tgsergeant@gmail.com>
2014-05-28 16:06:18 +1000

    Cleaned up tracing

--- hphp/runtime/base/tracing-collector.cpp
+++ hphp/runtime/base/tracing-collector.cpp
@@ -28,13 +28,10 @@ namespace HPHP {
 
 int64_t tracingGCCollect() {
   MarkSweepCollector msc;
-  msc.markHeap();
-  return 110;
+  return msc.markHeap();
 }
 
-#define cereal(x) std::cout << s.serialize(x, true).c_str()
-
-void MarkSweepCollector::markHeap() {
+int64_t MarkSweepCollector::markHeap() {
   Stack& stack = g_context->getStack();
   TypedValue *current = (TypedValue *)stack.getStackHighAddress();
 
@@ -44,8 +41,6 @@ void MarkSweepCollector::markHeap() {
     searchQ.push(*current);
     current --;
   }
-
-  std::cout << "Found " << searchQ.size() << " roots" << std::endl;
 
   TypedValue cur;
   VariableSerializer s(VariableSerializer::Type::VarDump);
@@ -59,41 +54,29 @@ void MarkSweepCollector::markHeap() {
     }
 
     if(cur.m_type == DataType::KindOfArray) {
-      std::cout << "Found an array" << std::endl;
-      cereal(Variant(cur.m_data.parr));
-
       for(ArrayIter iter(cur.m_data.parr); iter; ++iter) {
-        std::cout << "---" << std::endl;
-        cereal(iter.first());
-        cereal(iter.second());
-        TypedValue *second = iter.second().asTypedValue();
         searchQ.push(*iter.first().asTypedValue());
-        searchQ.push(*second);
+        searchQ.push(*iter.second().asTypedValue());
 
         markReachable(cur.m_data.parr);
       }
     }
 
     if(cur.m_type == DataType::KindOfRef) {
-      std::cout << "Found a ref" << std::endl;
       searchQ.push(*cur.m_data.pref->tv());
       markReachable(cur.m_data.pref);
     }
 
     if(cur.m_type == DataType::KindOfObject) {
-      std::cout << "Found an object" << std::endl;
       ObjectData *od = cur.m_data.pobj;
 
       auto const cls = od->getVMClass();
       const TypedValue *props = od->propVec();
       auto propCount = cls->numDeclProperties();
 
-      std::cout << "Class has " << propCount << " declared properties" << std::endl;
-
       //First go through all of the properties which are defined by the class
       for(auto i = 0; i < propCount; i++) {
         const TypedValue *prop = props + i;
-        std::cout << tname(prop->m_type) << std::endl;
         searchQ.push(*prop);
       }
 
@@ -102,9 +85,6 @@ void MarkSweepCollector::markHeap() {
         //The dynamic properties are implemented internally as an
         //array, so why not treat them in the same way?
         auto dynProps = od->dynPropArray();
-
-        std::cout << "Dynamic properties" << std::endl;
-        cereal(Variant(dynProps.get()));
 
         TypedValue dynTv;
         dynTv.m_data.parr = dynProps.get();
@@ -115,19 +95,17 @@ void MarkSweepCollector::markHeap() {
       markReachable(od);
     }
 
-    if(cur.m_type == DataType::KindOfStaticString) {
-      std::cout << "Found a static string" << std::endl;
-      cereal(Variant(cur.m_data.pstr));
+    if(cur.m_type == DataType::KindOfStaticString || cur.m_type == DataType::KindOfString) {
       markReachable(cur.m_data.pstr);
     }
 
-    if(cur.m_type == DataType::KindOfString) {
-      std::cout << "Found a string" << std::endl;
-      cereal(Variant(cur.m_data.pstr));
-      markReachable(cur.m_data.pstr);
+    if(cur.m_type == DataType::KindOfResource) {
+      std::cout << "Found a resource" << std::endl;
+      std::cout << "Not currently implemented" << std::endl;
+      markReachable(cur.m_data.pres);
     }
   }
-  std::cout << "Marked " << marked.size()  << " things" << std::endl;
+  return marked.size();
 }
 
 
