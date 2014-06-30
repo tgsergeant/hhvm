e2eafdb8171de1973fff5721a2c45aee491433f6
Tim Sergeant <tgsergeant@gmail.com>
2014-06-30 14:33:44 -0700

    Fix refcount survey against master, and fix indenting in those files

--- hphp/util/refcount-survey-impl.h
+++ hphp/util/refcount-survey-impl.h
@@ -39,75 +39,75 @@ RefcountSurvey &survey();
  * Where a unit of time is TIME_GRANULARITY operations
  */
 struct TimeDeltaActivity {
-	int deallocations;
-	int allocations;
-	long allocations_size;
+  int deallocations;
+  int allocations;
+  long allocations_size;
 };
 
 // Track data associated with a single object
 struct ObjectLifetimeData {
-	int max_refcount;
-	long allocation_time;
+  int max_refcount;
+  long allocation_time;
 };
 
 /*
  * Thread-local survey data and operations
  */
 struct RefcountSurvey {
-	typedef ThreadLocalSingleton<RefcountSurvey> TlsWrapper;
-	static void Create(void*);
-	static void Delete(RefcountSurvey*);
-	static void OnThreadExit(RefcountSurvey*);
-public:
-	void track_refcount_operation(RefcountOperation op, const void *address, int32_t value);
+  typedef ThreadLocalSingleton<RefcountSurvey> TlsWrapper;
+  static void Create(void*);
+  static void Delete(RefcountSurvey*);
+  static void OnThreadExit(RefcountSurvey*);
+  public:
+  void track_refcount_operation(RefcountOperation op, const void *address, int32_t value);
 
-	void track_refcount_request_end();
+  void track_refcount_request_end();
 
 private:
-	// 'Time' spent in this request so far
-	long total_ops = 0;
+  // 'Time' spent in this request so far
+  long total_ops = 0;
 
-	// Data about objects we know to be live in the heap
-	boost::unordered_map<const void *, ObjectLifetimeData> live_values;
+  // Data about objects we know to be live in the heap
+  boost::unordered_map<const void *, ObjectLifetimeData> live_values;
 
-	// Data for each time step
-	std::vector<TimeDeltaActivity> timed_activity;
+  // Data for each time step
+  std::vector<TimeDeltaActivity> timed_activity;
 
-	// The number of dead objects which reached each refcount value
-	Histogram<32> refcount_sizes;
+  // The number of dead objects which reached each refcount value
+  Histogram<32> refcount_sizes;
 
-	// Number of objects in each size bucket (freelists)
-	// 'Large' objects are lumped into the 128th slot
-	Histogram<129> object_sizes;
+  // Number of objects in each size bucket (freelists)
+  // 'Large' objects are lumped into the 128th slot
+  Histogram<129> object_sizes;
 
-	Histogram<250> object_lifetimes;
+  Histogram<250> object_lifetimes;
 
-	void track_change(const void *address, int32_t value);
-	/**
-	 * Record that a address has been released:
-	 *  - Remember the max value reached by this address
-	 *  - Mark that a release happened in the current time slot.
-	 */
-	void track_release(const void *address);
+  void track_change(const void *address, int32_t value);
+  /**
+   * Record that a address has been released:
+   *  - Remember the max value reached by this address
+   *  - Mark that a release happened in the current time slot.
+   */
+  void track_release(const void *address);
 
-	/**
-	 * Record that an address has been allocated:
-	 * - Track this in timed_activity
-	 * - Track the object size
-	 */
-	void track_alloc(const void *address, int32_t value);
+  /**
+   * Record that an address has been allocated:
+   * - Track this in timed_activity
+   * - Track the object size
+   */
+  void track_alloc(const void *address, int32_t value);
 
-	void increment_lifetime_bucket(long allocation_time);
+  void increment_lifetime_bucket(long allocation_time);
 
-	/**
-	 * Get or create a bucket for the current time slot
-	 */
-	TimeDeltaActivity *get_current_bucket();
+  /**
+   * Get or create a bucket for the current time slot
+   */
+  TimeDeltaActivity *get_current_bucket();
 
-	/**
-	 * Return this thread to its initial state at the end of a request
-	 */
-	void reset();
+  /**
+   * Return this thread to its initial state at the end of a request
+   */
+  void reset();
 };
 
 }
--- hphp/util/refcount-survey.cpp
+++ hphp/util/refcount-survey.cpp
@@ -34,237 +34,245 @@ folly::Synchronized<Histogram<129>, folly::RWSpinLock> global_object_sizes;
 
 //Need to initialise it before we do anything
 //We don't actually do anything with this object
-RefcountSurvey::TlsWrapper tls;
+static void *RefcountSurveyInit() {
+  RefcountSurvey::TlsWrapper tls;
+  return (void *) tls.getCheck();
+}
 
 // Return the survey object for the current thread
-RefcountSurvey &survey() { return *RefcountSurvey::TlsWrapper::getCheck(); }
+inline RefcountSurvey &survey() {
+  if (RefcountSurvey::TlsWrapper::isNull()) {
+    RefcountSurveyInit();
+  }
+  return *RefcountSurvey::TlsWrapper::getCheck();
+}
 
 void track_refcount_operation(RefcountOperation op, const void *address, int32_t value) {
-	survey().track_refcount_operation(op, address, value);
+  survey().track_refcount_operation(op, address, value);
 }
 
 void track_refcount_request_end() {
-	survey().track_refcount_request_end();
+  survey().track_refcount_request_end();
 }
 
 void dump_global_survey() {
-	SYNCHRONIZED_CONST(global_refcount_sizes) {
+  SYNCHRONIZED_CONST(global_refcount_sizes) {
 
-		long bitcounts[32] = {0};
-		int bc;
+    long bitcounts[32] = {0};
+    int bc;
 
-		long total_bits = 0;
-		long total_count = 0;
+    long total_bits = 0;
+    long total_count = 0;
 
-		TRACE(1, "\n\nRaw Refcount Sizes (across all requests)\n");
-		TRACE(1, "Size,Frequency\n");
-		TRACE(1, global_refcount_sizes.print());
-		for(int i = 0; i < 32; i++) {
-			bc = ceil(log2((double)i + 1));
-			bitcounts[bc] += global_refcount_sizes[i];
+    TRACE(1, "\n\nRaw Refcount Sizes (across all requests)\n");
+    TRACE(1, "Size,Frequency\n");
+    TRACE(1, global_refcount_sizes.print());
+    for(int i = 0; i < 32; i++) {
+      bc = ceil(log2((double)i + 1));
+      bitcounts[bc] += global_refcount_sizes[i];
 
-			total_bits += bc * global_refcount_sizes[i];
-			total_count += global_refcount_sizes[i];
-		}
+      total_bits += bc * global_refcount_sizes[i];
+      total_count += global_refcount_sizes[i];
+    }
 
-		double mean = (double)total_bits / total_count;
+    double mean = (double)total_bits / total_count;
 
-		long half = total_count / 2;
-		long half_sum = 0;
+    long half = total_count / 2;
+    long half_sum = 0;
 
-		double median;
+    double median;
 
-		for(int i = 0; i < 32; i++) {
-			half_sum += bitcounts[i];
-			if (half_sum > half) {
-				long pre = half_sum - bitcounts[i];
-				long diff = half - pre;
-				double proportion = (double)diff / bitcounts[i];
-				median = i - proportion;
-				break;
-			}
-		}
+    for(int i = 0; i < 32; i++) {
+      half_sum += bitcounts[i];
+      if (half_sum > half) {
+        long pre = half_sum - bitcounts[i];
+        long diff = half - pre;
+        double proportion = (double)diff / bitcounts[i];
+        median = i - proportion;
+        break;
+      }
+    }
 
-		TRACE(1, "\n\nRefcount Bits required\n");
-		TRACE(1, "Bits,Frequency\n");
-		for(int i = 0; i < 32; i++) {
-			if(bitcounts[i] > 0) {
-				FTRACE(1, "{},{}\n", i, bitcounts[i]);
-			}
-		}
-		TRACE(1, "\n");
-		FTRACE(1, "Mean: {}\n", mean);
-		FTRACE(1, "Median: {}\n", median);
-	}
+    TRACE(1, "\n\nRefcount Bits required\n");
+    TRACE(1, "Bits,Frequency\n");
+    for(int i = 0; i < 32; i++) {
+      if(bitcounts[i] > 0) {
+        FTRACE(1, "{},{}\n", i, bitcounts[i]);
+      }
+    }
+    TRACE(1, "\n");
+    FTRACE(1, "Mean: {}\n", mean);
+    FTRACE(1, "Median: {}\n", median);
+  }
 
-	SYNCHRONIZED_CONST(global_object_sizes) {
-		TRACE(1, "\n\nObject sizes (across all requests)\n");
-		TRACE(1, "Size,Frequency\n");
-		long total_bytes = 0;
-		long total_objects = 0;
-		for(int i = 0; i <= 128; i++) {
-			if(global_object_sizes[i] != 0) {
-				FTRACE(2, "{},{}\n", i << 4, global_object_sizes[i]);
-				total_bytes += (i << 4) * global_object_sizes[i];
-				total_objects += global_object_sizes[i];
-			}
-		}
-		TRACE(2, "\nTotal Objects,Total Bytes,Average Bytes\n");
-		FTRACE(2, "{},{},{:.1f}\n\n", total_objects, total_bytes, (double)total_bytes / total_objects);
-	}
+  SYNCHRONIZED_CONST(global_object_sizes) {
+    TRACE(1, "\n\nObject sizes (across all requests)\n");
+    TRACE(1, "Size,Frequency\n");
+    long total_bytes = 0;
+    long total_objects = 0;
+    for(int i = 0; i <= 128; i++) {
+      if(global_object_sizes[i] != 0) {
+        FTRACE(2, "{},{}\n", i << 4, global_object_sizes[i]);
+        total_bytes += (i << 4) * global_object_sizes[i];
+        total_objects += global_object_sizes[i];
+      }
+    }
+    TRACE(2, "\nTotal Objects,Total Bytes,Average Bytes\n");
+    FTRACE(2, "{},{},{:.1f}\n\n", total_objects, total_bytes, (double)total_bytes / total_objects);
+  }
 
 }
 
 void RefcountSurvey::Create(void *storage) {
-	new (storage) RefcountSurvey();
+  new (storage) RefcountSurvey();
 }
 
 void RefcountSurvey::Delete(RefcountSurvey *survey) {
-	survey->~RefcountSurvey();
+  survey->~RefcountSurvey();
 }
 
 void RefcountSurvey::OnThreadExit(RefcountSurvey *survey) {
-	TRACE(1, "~~~Survey thread died~~~");
-	survey->~RefcountSurvey();
+  TRACE(1, "~~~Survey thread died~~~");
+  survey->~RefcountSurvey();
 }
 
 void RefcountSurvey::track_refcount_request_end() {
-	//Processing that needs to be done before we print results
-	SYNCHRONIZED(global_refcount_sizes) {
-		global_refcount_sizes.add(refcount_sizes);
-	}
-	SYNCHRONIZED(global_object_sizes) {
-		global_object_sizes.add(object_sizes);
-	}
-	for(auto obj : live_values) {
-		increment_lifetime_bucket(obj.second.allocation_time);
-	}
+  //Processing that needs to be done before we print results
+  SYNCHRONIZED(global_refcount_sizes) {
+    global_refcount_sizes.add(refcount_sizes);
+  }
+  SYNCHRONIZED(global_object_sizes) {
+    global_object_sizes.add(object_sizes);
+  }
+  for(auto obj : live_values) {
+    increment_lifetime_bucket(obj.second.allocation_time);
+  }
 
-	// Then print the results (but only if the request was something interesting)
-	if(total_ops > 15000) {
-		TRACE(1, "\n-----Start of report-----\n");
+  // Then print the results (but only if the request was something interesting)
+  if(total_ops > 15000) {
+    TRACE(1, "\n-----Start of report-----\n");
 
-		auto const stats = MM().getStats();
-		TRACE(1, "\n\nGeneral memory statistics\n");
-		FTRACE(1, "Peak usage,\t\t{}\n", stats.peakUsage);
-		FTRACE(1, "Peak allocation,\t{}\n", stats.peakAlloc);
-		FTRACE(1, "Total allocation,\t{}\n", stats.totalAlloc);
+    auto const stats = MM().getStats();
+    TRACE(1, "\n\nGeneral memory statistics\n");
+    FTRACE(1, "Peak usage,\t\t{}\n", stats.peakUsage);
+    FTRACE(1, "Peak allocation,\t{}\n", stats.peakAlloc);
+    FTRACE(1, "Total allocation,\t{}\n", stats.totalAlloc);
 
-		dump_global_survey();
+    dump_global_survey();
 
-		TRACE(2, "\n\nMemory activity over time\nTimeslot,Releases,Allocations,Allocation Size\n");
-		for(int i = 0; i < timed_activity.size(); i++) {
-			auto r = timed_activity[i];
-			FTRACE(2, "{},{},{},{}\n", i + 1, r.deallocations, r.allocations, r.allocations_size);
-		}
-		FTRACE(2, "{},{},,\n", timed_activity.size() + 1, live_values.size());
-
-
-		TRACE(2, "\n\nObject lifetimes\n");
-		TRACE(2, "Time bucket,Frequency\n");
-		TRACE(2, object_lifetimes.print(false, LIFETIME_GRANULARITY));
-		FTRACE(2, "\nTotal operations: {}\n", total_ops);
-
-		TRACE(3, "\n\nRequest Object Sizes\n");
-		TRACE(3, "Size,Frequency\n");
-		TRACE(3, object_sizes.print(false, 16));
+    TRACE(2, "\n\nMemory activity over time\nTimeslot,Releases,Allocations,Allocation Size\n");
+    for(int i = 0; i < timed_activity.size(); i++) {
+      auto r = timed_activity[i];
+      FTRACE(2, "{},{},{},{}\n", i + 1, r.deallocations, r.allocations, r.allocations_size);
+    }
+    FTRACE(2, "{},{},,\n", timed_activity.size() + 1, live_values.size());
 
 
-		TRACE(1, "\n-----End of report-----\n\n");
-	}
+    TRACE(2, "\n\nObject lifetimes\n");
+    TRACE(2, "Time bucket,Frequency\n");
+    TRACE(2, object_lifetimes.print(false, LIFETIME_GRANULARITY));
+    FTRACE(2, "\nTotal operations: {}\n", total_ops);
 
-	reset();
+    TRACE(3, "\n\nRequest Object Sizes\n");
+    TRACE(3, "Size,Frequency\n");
+    TRACE(3, object_sizes.print(false, 16));
+
+
+    TRACE(1, "\n-----End of report-----\n\n");
+  }
+
+  reset();
 }
 
 
 void RefcountSurvey::track_release(const void *address) {
-	auto live_value = live_values.find(address);
-	if(live_value != live_values.end()) {
-		refcount_sizes.incr(live_value->second.max_refcount);
-		increment_lifetime_bucket(live_value->second.allocation_time);
-		live_values.erase(live_value);
-	}
-	get_current_bucket()->deallocations += 1;
+  auto live_value = live_values.find(address);
+  if(live_value != live_values.end()) {
+    refcount_sizes.incr(live_value->second.max_refcount);
+    increment_lifetime_bucket(live_value->second.allocation_time);
+    live_values.erase(live_value);
+  }
+  get_current_bucket()->deallocations += 1;
 }
 
 
 void RefcountSurvey::track_change(const void *address, int32_t value) {
-	if (value > 31) {
-		value = 31;
-	}
-	if (value < 0) {
-		return;
-	}
+  if (value > 31) {
+    value = 31;
+  }
+  if (value < 0) {
+    return;
+  }
 
-	auto live_value = live_values.find(address);
-	if (live_value == live_values.end()) {
-		live_values[address] = ObjectLifetimeData();
-		live_values[address].allocation_time = total_ops;
-		live_values[address].max_refcount = value;
-	}
-	else {
-		if(live_value->second.max_refcount < value) {
-			live_values[address].max_refcount = value;
-		}
-	}
+  auto live_value = live_values.find(address);
+  if (live_value == live_values.end()) {
+    live_values[address] = ObjectLifetimeData();
+    live_values[address].allocation_time = total_ops;
+    live_values[address].max_refcount = value;
+  }
+  else {
+    if(live_value->second.max_refcount < value) {
+      live_values[address].max_refcount = value;
+    }
+  }
 }
 
 void RefcountSurvey::track_refcount_operation(RefcountOperation op, const void *address, int32_t value) {
-	total_ops ++;
+  total_ops ++;
 
-	switch(op) {
-	case RC_RELEASE:
-		track_release(address);
-		break;
-	case RC_DEC:
-	case RC_INC:
-	case RC_SET:
-		track_change(address, value);
-		break;
-	case RC_ALLOC:
-		track_alloc(address, value);
-		break;
-	}
+  switch(op) {
+    case RC_RELEASE:
+      track_release(address);
+      break;
+    case RC_DEC:
+    case RC_INC:
+    case RC_SET:
+      track_change(address, value);
+      break;
+    case RC_ALLOC:
+      track_alloc(address, value);
+      break;
+  }
 }
 
 void RefcountSurvey::track_alloc(const void *address, int32_t value) {
-	TimeDeltaActivity *bucket = get_current_bucket();
-	bucket->allocations += 1;
-	bucket->allocations_size += value;
+  TimeDeltaActivity *bucket = get_current_bucket();
+  bucket->allocations += 1;
+  bucket->allocations_size += value;
 
-	int size;
-	if(value < 2048) {
-		size = ((value + 8) & ~15) >> 4;
-	} else {
-		size = 128;
-	}
-	object_sizes.incr(size);
+  int size;
+  if(value < 2048) {
+    size = ((value + 8) & ~15) >> 4;
+  } else {
+    size = 128;
+  }
+  object_sizes.incr(size);
 }
 
 void RefcountSurvey::increment_lifetime_bucket(long allocation_time) {
-	long lifetime = total_ops - allocation_time;
-	int lifetime_bucket = (int)((double)lifetime / LIFETIME_GRANULARITY) + 1;
-	if(lifetime_bucket > object_lifetimes.size()) {
-		lifetime_bucket = object_lifetimes.size() - 1;
-	}
-	object_lifetimes.incr(lifetime_bucket);
+  long lifetime = total_ops - allocation_time;
+  int lifetime_bucket = (int)((double)lifetime / LIFETIME_GRANULARITY) + 1;
+  if(lifetime_bucket > object_lifetimes.size()) {
+    lifetime_bucket = object_lifetimes.size() - 1;
+  }
+  object_lifetimes.incr(lifetime_bucket);
 }
 
 void RefcountSurvey::reset() {
-	refcount_sizes.clear();
-	object_sizes.clear();
-	live_values.clear();
-	object_lifetimes.clear();
-	timed_activity.clear();
-	total_ops = 0;
+  refcount_sizes.clear();
+  object_sizes.clear();
+  live_values.clear();
+  object_lifetimes.clear();
+  timed_activity.clear();
+  total_ops = 0;
 }
 
 TimeDeltaActivity *RefcountSurvey::get_current_bucket() {
-	int bucket = total_ops / TIME_GRANULARITY;
-	if(timed_activity.size() <= bucket) {
-		timed_activity.push_back(TimeDeltaActivity());
-	}
-	return &timed_activity[bucket];
+  int bucket = total_ops / TIME_GRANULARITY;
+  if(timed_activity.size() <= bucket) {
+    timed_activity.push_back(TimeDeltaActivity());
+  }
+  return &timed_activity[bucket];
 }
 
 ///////////////////////////////////////////////////////////////////////////////
