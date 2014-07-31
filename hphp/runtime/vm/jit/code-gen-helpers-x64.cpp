2480f948a8bda0ac519365374c6a5a6da62109f0
Tim Sergeant <tgsergeant@gmail.com>
2014-07-31 16:43:27 +1000

    Undo changes to jit surprise flags

--- hphp/runtime/vm/jit/code-gen-helpers-x64.cpp
+++ hphp/runtime/vm/jit/code-gen-helpers-x64.cpp
@@ -343,10 +343,9 @@ void emitTraceCall(CodeBlock& cb, int64_t pcOff) {
 }
 
 void emitTestSurpriseFlags(Asm& a) {
-  static_assert(RequestInjectionData::LastFlag < (1 << 16),
-                "Translator assumes RequestInjectionFlags fit in two bytes");
+  static_assert(RequestInjectionData::LastFlag < (1 << 8),
+                "Translator assumes RequestInjectionFlags fit in one byte");
   a.    testb((int8_t)0xff, rVmTl[RDS::kConditionFlagsOff]);
-  a.    testb((int8_t)0xff, rVmTl[RDS::kConditionFlagsOff + 1]);
 }
 
 void emitCheckSurpriseFlagsEnter(CodeBlock& mainCode, CodeBlock& stubsCode,
